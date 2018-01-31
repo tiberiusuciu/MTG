d69e0a4552fc871da9411d3f60cf6d3a803885e6
Tiberiu Cristian Suciu <tiberiu.cristian.suciu@gmail.com>
2018-01-30 20:56:13 -0500

    Base tabs added

--- app/components/Menu/MenuTools.js
+++ app/components/Menu/MenuTools.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css'
 
 import styles from '../../styles/Menu/MenuTools.css';
 
@@ -6,7 +8,28 @@ class MenuTools extends Component {
 	render() {
 		return (
 			<div className={styles.menuTools}>
-					Test
+				<Tabs>
+					<TabList>
+						<Tab>Stats</Tab>
+						<Tab>Hand</Tab>
+						<Tab>Chat</Tab>
+						<Tab>Menu</Tab>
+					</TabList>
+
+					<TabPanel>
+						<h2>Any content 1</h2>
+					</TabPanel>
+					<TabPanel>
+						<h2>Any content 2</h2>
+					</TabPanel>
+					<TabPanel>
+						<h2>Any content 3</h2>
+					</TabPanel>
+					<TabPanel>
+						<h2>Any content 4</h2>
+					</TabPanel>
+				</Tabs>
+
 			</div>
 		)
 	}
