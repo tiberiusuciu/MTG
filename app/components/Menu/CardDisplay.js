145d5eaf6f88a5f67333ed54ff1397fae61b370e
Tiberiu Cristian Suciu <tiberiu.cristian.suciu@gmail.com>
2018-02-05 06:56:40 -0500

    Fixed battlefield positionning, need some file cleaning now, and window sizing fixing

--- app/components/Menu/CardDisplay.js
+++ app/components/Menu/CardDisplay.js
@@ -6,10 +6,12 @@ class CardDisplay extends Component {
 	render() {
 		return (
 			<div className={styles.display}>
-					<img src="https://magiccards.info/scans/en/dgm/11.jpg" width="300px" height="400px" />
+					<img src="" width="300px" height="400px" />
 			</div>
 		)
 	}
 };
 
 export default CardDisplay;
+
+// https://magiccards.info/scans/en/dgm/11.jpg
