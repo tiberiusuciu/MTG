2fcdbb6af91e27c06155a0fb4168b18bdaf614e9
Tiberiu Cristian Suciu <tiberiu.cristian.suciu@gmail.com>
2018-02-05 07:10:17 -0500

    Brought back card image

--- app/components/Menu/CardDisplay.js
+++ app/components/Menu/CardDisplay.js
@@ -6,12 +6,10 @@ class CardDisplay extends Component {
 	render() {
 		return (
 			<div className={styles.display}>
-					<img src="" width="300px" height="400px" />
+					<img src="https://magiccards.info/scans/en/dgm/11.jpg" width="300px" height="400px" />
 			</div>
 		)
 	}
 };
 
 export default CardDisplay;
-
-// https://magiccards.info/scans/en/dgm/11.jpg
