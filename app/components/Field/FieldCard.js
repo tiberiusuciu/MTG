b8f2428a739465840aa94fd5afa98bcdcdccf2cb
Tiberiu Cristian Suciu <tiberiu.cristian.suciu@gmail.com>
2018-02-15 22:11:28 -0500

    Added card movement

--- app/components/Field/FieldCard.js
+++ app/components/Field/FieldCard.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
 
 // import FieldCard from './FieldCard';
+import Draggable from 'react-draggable';
 
 import styles from '../../styles/Field/FieldCard.css';
 
 class FieldCard extends Component {
+	eventLogger(e, data) {
+	    console.log('Event: ', e);
+	    console.log('Data: ', data);
+	}
+
 	render() {
 		return (
-			<div>
-				<img className={styles.card} src="https://magiccards.info/scans/en/nph/9.jpg"/>
-			</div>
+			<Draggable
+        axis="both"
+        handle=".handle"
+        defaultPosition={{x: 0, y: 24}}
+        position={null}
+        grid={[71, 100]}
+				bounds={{top: 24, left: 0, bottom: 324}}
+        onStart={this.handleStart}
+        onDrag={this.handleDrag}
+        onStop={this.handleStop}>
+				<div className="handle">
+					<img className={styles.card} src="https://magiccards.info/scans/en/nph/9.jpg" />
+				</div>
+			</Draggable>
 		)
 	}
 };
